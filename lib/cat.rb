5d84614cfaeb1f745f8e8ac278650f1985192a2f
Ben Parish <parishben13@gmail.com>
2020-06-01 04:15:14 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,13 @@
 class Cat
 attr_accessor :owner, :mood
 attr_reader :name
-
+@@all= []
 def initialize(name, owner)
   @name, @owner = name, owner
   @mood = "nervous"
+  @@all << self
+ end
+ def self.all 
+ @@all
  end
 end
