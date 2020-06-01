60c9738d6b6f64f253300b68f3422d5ccd9eacbd
Ben Parish <parishben13@gmail.com>
2020-06-01 03:58:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,12 @@
 class Owner
   attr_reader :name, :species
+  
   def initialize(name)
     @name = name
     @species = "human"
   end 
   
+  def say_species
+    puts "I am a #{self.species}."
+  end
 end
