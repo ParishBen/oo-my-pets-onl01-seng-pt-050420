48b1fbcd89cab6232a2d8443fe7ec257e0a1423e
Ben Parish <parishben13@gmail.com>
2020-06-01 04:17:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 class Dog
   attr_accessor :owner, :mood
   attr_reader :name
+  @@all = []
   def initialize(name, owner)
     @name, @owner = name, owner
     @mood = "nervous"
   end
+  def self.all 
+  @@all 
+ end
 end
