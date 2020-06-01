9813e2e9b59c0f6bc2177db431cd767118474a1c
Ben Parish <parishben13@gmail.com>
2020-06-01 04:08:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,16 @@
 class Owner
   attr_reader :name, :species
-  
+  @@all= []
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end 
   
   def say_species
      "I am a #{@species}."
   end
+  self.all
+  @@all
+end
 end
